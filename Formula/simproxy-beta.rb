f1efc29afee6a32155c8aacdfa2f679564dd17a9
bharath2020 <903536+bharath2020@users.noreply.github.com>
2026-04-06 22:43:30 -0700

    Add simproxy beta formula

---
+++ Formula/simproxy-beta.rb
@@ -0,0 +1,21 @@
+class SimproxyBeta < Formula
+  include Language::Python::Virtualenv
+
+  desc "Intercept and inspect iOS Simulator network traffic from the command line"
+  homepage "https://github.com/bharath2020/simproxy"
+  url "https://github.com/bharath2020/simproxy/archive/refs/tags/v0.1.0-beta.1.tar.gz"
+  sha256 "4ce7ea97ce6e552184267c80b998670765e1276b8bfb70d9b26de0de9608122f"
+  license "MIT"
+  head "https://github.com/bharath2020/simproxy.git", branch: "main"
+
+  depends_on "mitmproxy"
+  depends_on "python@3.13"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match "simproxy", shell_output("#{bin}/simproxy --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# homebrew-tools
+
+Homebrew tap for Bharath's utilities.
+
+## Install
+
+```bash
+brew tap bharath2020/tools
+brew install simproxy-beta
+```
